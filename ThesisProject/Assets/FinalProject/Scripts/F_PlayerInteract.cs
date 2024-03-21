66ba88464fd2e44b06d203d8b5efae9c58b58c8d
SaraGolob <saragolob@hotmail.com>
2024-03-21 13:53:54 +0100

    Interaction with training dummy

---
+++ ThesisProject/Assets/FinalProject/Scripts/F_PlayerInteract.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Interface for all objects the player can interact with
+public interface IInteractable
+{
+    void Interact();
+}
+public class F_PlayerInteract : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float interactRange;
+    private void OnInteract(InputValue value)
+    {
+        // Shooting a ray from our camera
+        Ray ray = new Ray
+        {
+            origin = playerCamera.transform.position,
+            direction = playerCamera.transform.forward
+        };
+
+        // If you want to see the ray when you press interact button you can uncomment this line of code.
+        //Debug.DrawRay(ray.origin, ray.direction* interactRange);
+
+        // Saving hit information and checking if we hit an interactable object
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, interactRange))
+        {
+            IInteractable interactable = hitInfo.collider.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                // Calling interact method of the interactable
+                interactable.Interact();
+            }
+        }
+    }
+}
---
+++ ThesisProject/Assets/FinalProject/Scripts/F_TrainingDummy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_TrainingDummy : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("Hello! I am a training dummy");
+    }
+}

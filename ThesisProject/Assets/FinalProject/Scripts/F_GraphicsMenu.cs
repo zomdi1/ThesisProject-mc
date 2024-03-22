4eb6ded2675a668b37e913cca65b8aeb3ab98e28
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2024-03-22 11:16:27 +0100

    audio menu . Pause MEnu finished

--- ThesisProject/Assets/FinalProject/Scripts/F_GraphicsMenu.cs
+++ ThesisProject/Assets/FinalProject/Scripts/F_GraphicsMenu.cs
@@ -9,10 +9,12 @@ public class F_GraphicsMenu : MonoBehaviour
     [SerializeField] TMP_Dropdown resolutionDropDown; //a reference to the player object to gain access to its components
     [SerializeField] Slider mouseSensitivitySlider;//a reference to the player object to gain access to its components
     [SerializeField] TMP_Text sensitivityValueTxt;//a reference to the player object to gain access to its components
+    Resolution[] resolutions ;
     // Start is called before the first frame update
     void Start()
     {
         playerObject = GameObject.FindGameObjectWithTag("Player");
+        PopulateResDropDown();
     }
 
     public void ChangeSensitivity()
@@ -25,5 +27,31 @@ public class F_GraphicsMenu : MonoBehaviour
     public void ToggleFullScreen()
     {
          Screen.fullScreen = !Screen.fullScreen;
+    }
+    public void PopulateResDropDown()
+    {
+        int currentResIndex = 0;
+
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width+ "x" + resolutions[i].height );
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropDown.ClearOptions();
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    public void ChangeResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }

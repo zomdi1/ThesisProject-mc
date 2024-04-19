7b67d82b70caf532ac6ae1dee7bbbd90fb353a3f
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2024-04-19 09:05:21 +0200

    armor stand fix

--- ThesisProject/Assets/FinalProject/Scripts/F_AnimationArmorStand.cs
+++ ThesisProject/Assets/FinalProject/Scripts/F_AnimationArmorStand.cs
@@ -4,26 +4,9 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public interface IDamageable
-{
-    public int Health { get;  set; }
-    public Slider healthBar { get; set; }
-    public bool canTakeDamage { get; set; }
-    public IEnumerator DamageCoolDown(int damageDelay);
-    public void TakeDamage(int damageAmount);
-}
-public class F_AnimationArmorStand : MonoBehaviour,IInteractable, IDamageable
-{
-    [field:SerializeField] public int Health { get; set; }
-    [field: SerializeField] public Slider healthBar { get ; set ; }
-    public bool canTakeDamage { get; set; } = true;
 
-    public IEnumerator DamageCoolDown(int damageDelay)
-    {
-        yield return new WaitForSeconds(damageDelay);
-        canTakeDamage = true;
-    }
-
+public class F_AnimationArmorStand : MonoBehaviour,IInteractable
+{
     public void Interact()
     {
         if (GetComponent<Animation>().isPlaying)
@@ -31,17 +14,5 @@ public class F_AnimationArmorStand : MonoBehaviour,IInteractable, IDamageable
             return;
         }
         GetComponent<Animation>().Play();
-    }
-
-    public void TakeDamage(int damageAmount)
-    {
-        if (canTakeDamage)
-        {
-            canTakeDamage = false;
-            Health -= damageAmount;
-            Debug.Log(Health.ToString());
-            //healthBar.value = Health;
-            StartCoroutine(DamageCoolDown(2));
-        }
     }
 }

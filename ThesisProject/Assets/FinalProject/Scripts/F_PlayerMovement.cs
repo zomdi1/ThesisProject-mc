a664e51b3598d805e7722ac68222ad00f8543429
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2024-04-21 10:19:07 +0200

    playermovement fixed

--- ThesisProject/Assets/FinalProject/Scripts/F_PlayerMovement.cs
+++ ThesisProject/Assets/FinalProject/Scripts/F_PlayerMovement.cs
@@ -9,7 +9,7 @@ public class F_PlayerMovement : MonoBehaviour
     private Rigidbody characterRB; // Reference to the Rigidbody component of the character
 
     private Vector3 movementInput; // Stores movement input received from the player
-    public Vector3 movementVector { get; private set; } // Stores the resulting movement vector
+    public Vector3 movementVector; // Stores the resulting movement vector
     private Animator animator; //Animator to turn on animations
     [SerializeField] private float movementSpeed; // Movement speed of the character
 
@@ -27,6 +27,7 @@ public class F_PlayerMovement : MonoBehaviour
         {
             // Calculate movement vector based on input and current orientation of the character
             movementVector = transform.right * movementInput.x + transform.forward * movementInput.z;
+            movementVector.y = 0; //since we are rotating the entire game object we only want to calculate the movement vector along the x and z axis (ignore the vertical component of movement)
         }
 
         // Set the velocity of the character's Rigidbody to move it
